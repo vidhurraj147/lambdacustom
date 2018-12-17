22b488560d1dab1f8d766e22d32766c3476d121f
vidhurraj147 <vidhurraj147@gmail.com>
2018-12-17 00:18:52 -0500

    Basic lambda custom handler

---
+++ lambdacustom/src/main/java/com/amazonaws/lambda/lambdacustom/LambdaFunctionHandler.java
@@ -0,0 +1,16 @@
+package com.amazonaws.lambda.lambdacustom;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        return "Hello from Lambda! THIS IS LAMBDACUSTOM --> "+input ;
+    }
+
+}
